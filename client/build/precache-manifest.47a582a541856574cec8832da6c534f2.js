865c1ca6f364ee25f43e40db687459f85de7e44d
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-25 10:57:38 -0700

    deploy build thing

---
+++ client/build/precache-manifest.47a582a541856574cec8832da6c534f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f93ff04aa88ec00be30",
+    "url": "/google-books/static/js/runtime~main.6b0629ed.js"
+  },
+  {
+    "revision": "cb62db5a367b48a8c462",
+    "url": "/google-books/static/js/main.83f32132.chunk.js"
+  },
+  {
+    "revision": "8c236ad5c5004b82e8d6",
+    "url": "/google-books/static/js/2.8110ee36.chunk.js"
+  },
+  {
+    "revision": "cb62db5a367b48a8c462",
+    "url": "/google-books/static/css/main.ebbed938.chunk.css"
+  },
+  {
+    "revision": "5b4fa096c1143cdead2038449649ecdd",
+    "url": "/google-books/index.html"
+  }
+];
