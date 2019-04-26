a2391ce2f9218d58b2e7bd6e50e766904188945e
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-26 11:06:57 -0700

    Updates

---
+++ precache-manifest.a46d4d643eefe4298181691460fd9985.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f93ff04aa88ec00be30",
+    "url": "/google-books/static/js/runtime~main.6b0629ed.js"
+  },
+  {
+    "revision": "e6d168e6fbb2c6bcda4b",
+    "url": "/google-books/static/js/main.850a7ae3.chunk.js"
+  },
+  {
+    "revision": "49d0a1d615d6022a7229",
+    "url": "/google-books/static/js/2.da956ba8.chunk.js"
+  },
+  {
+    "revision": "e6d168e6fbb2c6bcda4b",
+    "url": "/google-books/static/css/main.cbe984a1.chunk.css"
+  },
+  {
+    "revision": "447b1d5739e5ac3a5a6d6558cb7cd323",
+    "url": "/google-books/index.html"
+  }
+];
